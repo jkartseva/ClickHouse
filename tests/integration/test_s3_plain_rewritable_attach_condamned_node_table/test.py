745b032c1198e23caa1c943014232a786d546904
Julia Kartseva <yulia.kartseva@gmail.com>
2025-04-02 03:01:23 +0000

    backup

--- tests/integration/test_s3_plain_rewritable_attach_condamned_node_table/test.py
+++ tests/integration/test_s3_plain_rewritable_attach_condamned_node_table/test.py
@@ -1,10 +1,12 @@
 import logging
 import random
 import string
+import uuid
 
 import pytest
 
 from helpers.cluster import ClickHouseCluster
+from helpers.config_cluster import minio_secret_key
 
 cluster = ClickHouseCluster(__file__)
 
@@ -71,27 +73,54 @@ def test():
     uuid2 = node1.query("SELECT uuid FROM system.tables WHERE table='test2'").strip()
     logging.info(f"UUID {uuid2}")
 
-    node1.query("DETACH TABLE test1")
+    # node1.query("DETACH TABLE test1")
     node1.query("DETACH TABLE test2")
-    node1.stop()
+    # node1.stop()
 
     node2 = cluster.instances["node2"]
-    for (name, uuid) in [('test1', uuid1), ('test2', uuid2)]:
+    for (name, uuid_s) in [('test1', uuid1), ('test2', uuid2)]:
         node2.query(
-        f'''ATTACH TABLE test_rotated_{name} UUID '{uuid}' (id Int64, data String)
+        f'''ATTACH TABLE test_rotated_{name} UUID '{uuid_s}' (id Int64, data String)
         ENGINE=MergeTree()
         ORDER BY id
         SETTINGS disk=disk(
+            # readonly=true,
             name=my_disk,
-            type=object_storage,
-            object_storage_type=s3,
-            metadata_type=plain_rewritable,
-            endpoint='http://minio1:9001/root/data/',
-            endpoint_subpath='node1',
+            type='s3_plain_rewritable',
+            endpoint='http://minio1:9001/root/data/node1',
             access_key_id='minio',
-            secret_access_key='minio123')
+            secret_access_key='ClickHouse_Minio_P@ssw0rd')
         ''')
+
+    backup_destination = (
+        f"S3('http://minio1:9001/root/data/backups/backup', 'minio', '{minio_secret_key}')"
+    )
+    backup_query_id = uuid.uuid4().hex
+
+    node2.query(
+        f"BACKUP TABLE test_rotated_test1 TO {backup_destination}",
+        query_id=backup_query_id,
+    )
 
     assert int(node2.query("SELECT count(*) FROM test_rotated_test1")) == 1000
     assert int(node2.query("SELECT count(*) FROM test_rotated_test2")) == 1000
 
+    node2.query("DROP TABLE test_rotated_test1 SYNC")
+    node1.query("DROP TABLE test1 SYNC")
+
+    restore_query_id = uuid.uuid4().hex
+    node2.query(
+        f"""
+        RESTORE TABLE test_rotated_test1 AS data_restored FROM {backup_destination};
+        """,
+        query_id=restore_query_id,
+    )
+
+    r=node2.query(
+        f"""
+        SELECT create_table_query FROM system.tables WHERE name='data_restored' SETTINGS format_display_secrets_in_show_and_select=1
+        """)
+    print(r)
+
+    assert int(node2.query("SELECT count(*) FROM data_restored")) == 1000
+    assert int(node2.query("SELECT count(*) FROM test_rotated_test2")) == 1000
